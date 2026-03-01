555ccffdf8affcab202a736a4158f49c3ebda246
lukagbn <luka68690@gmail.com>
2026-03-01 14:53:16 +0400

    style: change styles for camera-container on panda page

--- online-zoo/pages/panda/app.js
+++ online-zoo/pages/panda/app.js
@@ -3,16 +3,24 @@ const asideToggle = document.querySelector(".arrow-icon");
 const carousel = document.querySelector(".carousel-inner");
 const prevBtn = document.querySelector(".carousel button:first-child");
 const nextBtn = document.querySelector(".carousel button:last-child");
-
+const carouselItems = document.querySelectorAll(
+  ".carousel-inner .carousel-item",
+);
 asideToggle.addEventListener("click", () => {
   aside.classList.toggle("active");
 });
 
-const scrollAmount = carousel.offsetWidth * 0.3;
+const scrollAmount = carousel.offsetWidth * 0.4;
 
 prevBtn.addEventListener("click", () => {
   carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
 });
 nextBtn.addEventListener("click", () => {
   carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+});
+carouselItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    carouselItems.forEach((el) => el.classList.remove("active"));
+    item.classList.toggle("active");
+  });
 });
