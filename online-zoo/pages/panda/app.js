0571f8b8a2d7c8d11892808821f7c46b4030cc2d
lukagbn <luka68690@gmail.com>
2026-03-01 13:53:15 +0400

    feat: add favicon and slider functionallity to panda page

--- online-zoo/pages/panda/app.js
+++ online-zoo/pages/panda/app.js
@@ -1,6 +1,18 @@
 const aside = document.querySelector("aside");
 const asideToggle = document.querySelector(".arrow-icon");
+const carousel = document.querySelector(".carousel-inner");
+const prevBtn = document.querySelector(".carousel button:first-child");
+const nextBtn = document.querySelector(".carousel button:last-child");
 
 asideToggle.addEventListener("click", () => {
   aside.classList.toggle("active");
+});
+
+const scrollAmount = carousel.offsetWidth * 0.3;
+
+prevBtn.addEventListener("click", () => {
+  carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+});
+nextBtn.addEventListener("click", () => {
+  carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
 });
